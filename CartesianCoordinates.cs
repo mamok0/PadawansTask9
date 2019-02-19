5d5f15e5e0f73f14c6dfc1f9dbdb47d542cd612f
Artem Kivachuk <mamokosf@gmail.com>
2019-02-19 20:18:04 +0300

    first solving

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PadawansTask9
 {
@@ -7,8 +8,19 @@ namespace PadawansTask9
     {
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (range < 0)
+                throw new ArgumentException();
+            if (points == null)
+                throw new ArgumentNullException();
+
+            List<Point> neighbours = new List<Point>();
+
+            foreach (Point somePoint in points)
+            {
+                if (Math.Abs(somePoint.X - point.X)<=range && Math.Abs(somePoint.Y - point.Y) <= range)
+                    neighbours.Add(somePoint);
+            }
+            return neighbours;
         }
     }
 }
